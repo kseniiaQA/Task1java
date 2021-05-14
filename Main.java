2e7eedde94fd44aa3ec517545b0b7bf92e206367
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-05-14 11:59:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 30;
         int forOneMile = 20;
-
         int bonus;
+        
         bonus = ticketPrice / forOneMile;
         System.out.println("Ваш бонус = " + bonus);
     }
